56e7eaedcc686303cf3f04764b57842fa496c9d4
Hazel Jane Lihotan <hjlihotan@gmail.com>
2026-08-21 07:48:51 +0800

    added config/config.php and config/functions.php

--- config/config.php
+++ config/config.php
@@ -1,41 +1,36 @@
 <?php
-session_start();
+    session_start();
+
+require_once(__DIR__ . '/../includes/activity-logger.php');
 
 // define('','');
-define('BASE URL', http://localhost/IT34A/');
+define('BASE_URL','http://localhost/it34a');
 
-define('DB_HOST', 'localhost');
-define('DN_NAME', 'it34a_lab_db');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST','localhost');
+define('DB_NAME','it34a_lab_db');
+define('DB_USER','root');
+define('DB_PASS','');
 
 $user_id = "root" ?? null;
 $user_email = "root" ?? null;
 
-try(
+try{
     $pdo = new PDO(
-        "mysql:host=".DB_HOST." ;dbname=" . DB_NAME, 
-        DB_USER, 
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
+        DB_USER,
         DB_PASS,
-        [PDO::ATTR_ERRMODE -> PDO::ERRMODE_EXCEPTION]   
-);
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
 
-    $success = logActivity($pdo,$user_id,$email,'connection_db','success');
-
-    if($success){
-        echo "Activity log inserted succesfully";
-    }else{
-        echo "Failed to insert activity log";
-    }
-
-    // echo('Connection Successful');
-    // echo()
-
-    //logActivity($pdo,$user_id,$email,'connection_db','success');
-
+    echo ("Connection Successful");
+    echo ($user_id . $user_email .'connect_db'.'success');
+    logActivity($pdo,$user_id,$user_email,'connect_db','success');
 
 }catch(PDOException $e){
-    die('Connection Failed: ' . $e->getMessage());
+    die("Connection failed: " . $e->getMessage());
+    
 }
 
-?>
+ ?>
+
+

dda2944b65630d84acd8b732fa655e76b2429d1e
Hazel Jane Lihotan <hjlihotan@gmail.com>
2026-08-20 13:31:18 +0800

    added config/config.php and config/functions.php

---
+++ config/config.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+// define('','');
+define('BASE URL', http://localhost/IT34A/');
+
+define('DB_HOST', 'localhost');
+define('DN_NAME', 'it34a_lab_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+$user_id = "root" ?? null;
+$user_email = "root" ?? null;
+
+try(
+    $pdo = new PDO(
+        "mysql:host=".DB_HOST." ;dbname=" . DB_NAME, 
+        DB_USER, 
+        DB_PASS,
+        [PDO::ATTR_ERRMODE -> PDO::ERRMODE_EXCEPTION]
+);
+    // echo('Connection Successful');
+    // echo()
+
+    logActivity($pdo,$user_id,$email,'connection_db','success');
+
+
+}catch(PDOException $e){
+    die('Connection Failed: ' . $e->getMessage());
+}
+
+?>

ded727076199d14406c718d1b546c6c20cd5053d
Hazel Jane Lihotan <hjlihotan@gmail.com>
2026-09-15 13:37:13 +0800

    Added activity logging and signout

--- app/auth/signout.php
+++ app/auth/signout.php
@@ -3,6 +3,15 @@
 // Signout
 require_once __DIR__ . '/../../config/config.php';
 
+if(isset($_SESSION['user_id'])){
+    logActivity(
+        $pdo,
+        $_SESSION['user_id'],
+        $_SESSION['user_email'],
+        'logout',
+        'success'
+    );
+}
 $_SESSION = [];
 session_destroy();
 
